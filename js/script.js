6ffceca87e118f6051a350a5d98592b29001f183
Nikita <nikita_rs7@ukr.net>
2023-06-18 01:55:53 +0300

    newblock

--- js/script.js
+++ js/script.js
@@ -17,12 +17,53 @@
     nextEl: '.portfolio__arrow_right',
     prevEl: '.portfolio__arrow_left',
   },
-  slidesPerView:3,
-  spaceBetween:56,
+  slidesPerView:1.2,
+  spaceBetween:30,
   speed:800,
   observer:true,
   observeParents:true,
+   breakpoints: {
+   
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 56,
+    },
+    // when window width is >= 480px
+     480: {
+      slidesPerView: 2,
+     },
+ }
 })  
+
+const tabsBtn = document.querySelectorAll(".experience__nav-btn");
+const tabsItems = document.querySelectorAll(".content-experience__body");
+tabsBtn.forEach(function(item){
+  item.addEventListener("click",function(){
+
+   let currentBtn = item;
+   let tabId = currentBtn.getAttribute("data-tab");
+   let currentTab = document.querySelector(tabId);
+
+   if( ! currentBtn.classList.contains('active')){
+ tabsBtn.forEach(function(item){
+      item.classList.remove('active');
+      item.classList.remove('_icon-arrow');
+   });
+
+   tabsItems.forEach(function(item){
+    item.classList.remove('active');
+   });
+
+   currentBtn.classList.add('active');
+   currentBtn.classList.add('_icon-arrow');
+   currentTab.classList.add('active');
+   }
+
+  
+  });
+});
+
+document.querySelector('.experience__nav-btn').click();
    
 
 
